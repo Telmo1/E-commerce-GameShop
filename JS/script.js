3be67c1c5198bd6dcaf6ed04f5518c5081f0ef36
Telmo <yrtelmo00@gmail.com>
2023-03-17 19:52:42 -0300

    Implementando media query/1

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+let cabecalho = document.getElementById("menu");
+
+function mostrarCabecalho(){
+    //Ações 
+    // Se, o meu menur n~so estiver aparecendo, ele irá aparecer
+    if(cabecalho.style.display == "none" ){
+
+        cabecalho.style.display = "flex";
+    } else {
+        cabecalho.style.display = "none";
+    }
+
+    //Então, se o menur estiver aparecendo, ele irá desaparecer
+}
